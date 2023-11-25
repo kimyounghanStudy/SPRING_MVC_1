efbf203b0520fb35d7549271367ce6de4b284086
J-Jaeh <1wo2gur@naver.com>
2023-11-25 22:58:05 +0900

    [feat] adapter 패턴을 이용한 v3연결
    - adapter 패턴을 이용해서 다양한 컨트롤러를 사용가능하게 할수있다
    - 그렇게 하기 위해선 . URL 주소를 맵핑 시켜줄것과, 해당 어뎁터를 반환시켜줄 것이 필요

---
+++ src/main/java/hello/servlet/web/frontcontroller/v5/FrontControllerServletV5.java
@@ -0,0 +1,70 @@
+package hello.servlet.web.frontcontroller.v5;
+
+import hello.servlet.web.frontcontroller.ModelView;
+import hello.servlet.web.frontcontroller.MyView;
+import hello.servlet.web.frontcontroller.V3.controller.MemberFormControllerV3;
+import hello.servlet.web.frontcontroller.V3.controller.MemberListControllerV3;
+import hello.servlet.web.frontcontroller.V3.controller.MemberSaveControllerV3;
+import hello.servlet.web.frontcontroller.v5.adapter.ControllerV3HandlerAdapter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@WebServlet(name = "frontControllerServletV5", urlPatterns = "/front-controller/5/*")
+public class FrontControllerServletV5 extends HttpServlet {
+    private final Map<String, Object> handlerMappingMap = new HashMap<>();//final 은 중요하지 않음..
+    private final List<MyHandlerAdapter> handlerAdapters = new ArrayList<>();
+
+    public FrontControllerServletV5() {
+        initHandlerMappingMap();
+        initHandlerAdapters();
+    }
+    private void initHandlerMappingMap() {
+        // 오류 뜨면 콘솔창 로그를 확인해보자 !
+        handlerMappingMap.put("/front-controller/v5/v3/members/new-form", new MemberFormControllerV3());
+        handlerMappingMap.put("/front-controller/v5/v3/members/save", new MemberSaveControllerV3());
+        handlerMappingMap.put("/front-controller/v5/v3/members", new MemberListControllerV3());
+    }
+    private void initHandlerAdapters() {
+        handlerAdapters.add(new ControllerV3HandlerAdapter());
+    }
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Object handler = getHandler(request);
+
+        if (handler == null) {
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+        MyHandlerAdapter adapter =getHandlerAdapter(handler);
+        ModelView modelView = adapter.handle(request, response, handler);
+        String viewName = modelView.getViewName();
+        MyView view = viewResolver(viewName);
+        view.render(modelView.getModel(),request,response);
+    }
+
+    private Object getHandler(HttpServletRequest request) {
+        String requestURI = request.getRequestURI();
+        return handlerMappingMap.get(requestURI);
+    }
+    private MyHandlerAdapter getHandlerAdapter(Object handler) {
+        for (MyHandlerAdapter adapter : handlerAdapters) {
+            if (adapter.supports(handler)) {
+                return adapter;
+            }
+        }
+        throw new IllegalArgumentException("handler adapter를 찾을 수 없습니다. handler= " + handler);
+    }
+    private static MyView viewResolver(String mv) {
+        return new MyView("/WEB-INF/views/" + mv + ".jsp");
+    }
+}
